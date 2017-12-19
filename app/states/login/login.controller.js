9078805d53becca574c19f823ef38a35cd3a2239
Sonam <sonam@Sonams-MacBook-Pro.local>
2017-12-19 13:22:51 +0530

    Loader added

--- app/states/login/login.controller.js
+++ app/states/login/login.controller.js
@@ -1,18 +1,22 @@
 (function () {
-    angular.module("starwars").controller('LoginController', ['ApiService', 'UserService', '$state', function(ApiService, UserService, $state){
+    angular.module("starwars").controller('LoginController', ['ApiService', 'UserService', '$state', '$timeout', function(ApiService, UserService, $state, $timeout){
         var vm = this;
+        vm.loading = false;
 
         vm.login = function(){
-            ApiService.login({search : vm.username},function(r){
-                for(let i = 0; i< r.results.length; i++ ){
-                    if(r.results[i].name === vm.username && r.results[i].birth_year === vm.password){
-                        UserService.setUser(r.results[i]);
-                        $state.go('home.main');
+            vm.loading = true;
+            $timeout( function(){
+                ApiService.login({search : vm.username},function(r){
+                    for(let i = 0; i< r.results.length; i++ ){
+                        if(r.results[i].name === vm.username && r.results[i].birth_year === vm.password){
+                            UserService.setUser(r.results[i]);
+                            $state.go('home.main');
+                        }
                     }
-                }
-            }, function(err){
-                console.log(err);
-            });
+                }, function(err){
+                    console.log(err);
+                });
+            }, 2000 );
         }
 
     }]);
